c3218e2ac70599ef52220cf594c40b77e698f0ce
Kyungyun Kim <kyknn79@naver.com>
2020-08-27 13:34:35 +0900

    commit

--- shoppingmallclone/shop_login/views.py
+++ shoppingmallclone/shop_login/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import UserInfo
+
+from django.views import generic
 
 # Create your views here.
+class Shop_login(generic.TemplateView)/:
+    def get(self, request, *args, **kwargs):
+        template_name = 'shop_login/login.html'
+        user_info = UserInfo.objects.all()
+        return render(request, template_name, {"user_info":user_info})
